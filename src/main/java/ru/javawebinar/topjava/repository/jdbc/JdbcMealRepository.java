b13ed6b571504da0133c560c9dfc7965fbb7cc02
Mykhailo Hurin <hurindiplommail@gmail.com>
2021-07-13 11:18:01 +0300

    done HW_03

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,8 +1,14 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.repository.rowmapper.MealMapper;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,28 +16,62 @@ import java.util.List;
 @Repository
 public class JdbcMealRepository implements MealRepository {
 
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final SimpleJdbcInsert insertMeal;
+
+    @Autowired
+    public JdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.insertMeal = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("date_time", meal.getDateTime())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories())
+                .addValue("user_id", userId);
+        if (meal.isNew()) {
+            Number newId = insertMeal.executeAndReturnKey(map);
+            meal.setId(newId.intValue());
+        } else {
+            if (namedParameterJdbcTemplate.update(
+                    "UPDATE meals SET description=:description, calories=:calories, date_time=:date_time " +
+                            "WHERE id=:id AND user_id=:user_id", map) == 0)
+                return null;
+        }
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM meals WHERE id=? AND user_id=?", id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM meals WHERE id=?",
+                new Object[]{id},
+                new MealMapper()).stream().findAny().orElse(null);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id=? ORDER BY date_time DESC ",
+                new MealMapper(), userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return jdbcTemplate.query(
+                "SELECT * FROM meals WHERE user_id=? AND date_time >= ? AND date_time < ? ORDER BY date_time DESC",
+                new MealMapper(), userId, startDateTime, endDateTime);
     }
 }
---
+++ src/main/java/ru/javawebinar/topjava/repository/rowmapper/MealMapper.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.topjava.repository.rowmapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.javawebinar.topjava.model.Meal;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MealMapper implements RowMapper<Meal> {
+    @Override
+    public Meal mapRow(ResultSet resultSet, int i) throws SQLException {
+        Meal meal = new Meal();
+        meal.setId(resultSet.getInt("id"));
+        meal.setDateTime(resultSet.getTimestamp("date_time").toLocalDateTime());
+        meal.setDescription(resultSet.getString("description"));
+        meal.setCalories(resultSet.getInt("calories"));
+        return meal;
+    }
+}
